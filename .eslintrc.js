90c33d4ef928d9efe72bb02414ae58c31f5454e5
TonyFreelance <tony@tonyfreelance.com>
2016-08-01 08:54:15 +0700

    Update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,13 @@ module.exports = {
     ],
     "rules":{
         "no-console":0,
-        "indent": 0
+        "indent": 0,
+        "react/jsx-indent": [2, "tab"],
+        "react/jsx-indent-props": [2, "tab"],
+        "no-trailing-spaces": ["error", { "skipBlankLines": true }],
+        "object-curly-spacing": ["error", "never"],
+        "max-len": [1, 120, 2, {ignoreComments: true}],
+        "quote-props": [1, "consistent-as-needed"],
+
     },
-    "indent": ["error", "tab"]
 };
